a2bada2dbbc471b5b3e39a50ddf797cd5d209812
twaasoulElm3refa <twaslm3rfa@gmail.com>
2026-04-16 15:03:39 +0200

    create withdrawl pages ( all - single - filter by status - update)

---
+++ resources/js/views/admin/withdraw/withdrawl_status.vue
@@ -0,0 +1,12 @@
+<template>
+    <AdminLayout>
+        <div>
+            <h1>withdraw status</h1>
+        </div>
+    </AdminLayout>
+
+</template>
+
+<script setup>
+import AdminLayout from '../../../layouts/AdminLayout.vue';
+</script>
